69b6e5e34da4c3d7315603ee57f70b6a69f39715
Mark <f4nt@f4ntasmic.com>
2009-05-18 12:27:02 -0500

    few feed changes

--- blog/feeds.py
+++ blog/feeds.py
@@ -1,11 +1,12 @@
+from django.conf import settings
 from django.contrib.syndication.feeds import Feed
 from django_yaba.blog.models import Story
 
 class RSSFeed(Feed):
-    title = "..::f4ntasmic studios::.."
+    title = settings.BLOG_NAME
     description = "Recent Posts"
-    link = "http://www.django_yaba.com/"
-    item_link = link
+    #link = settings.ROOT_BLOG_URL
+    #item_link = link
 
     def items(self):
         return Story.objects.all().order_by('-created')[:10]
