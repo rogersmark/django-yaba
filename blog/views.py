eb7edc81ab10ef11cba526e4ca522711a0955902
Mark <f4nt@f4ntasmic.com>
2009-06-14 00:04:26 -0500

    galleries and posts are both displayed on the front page now

--- blog/views.py
+++ blog/views.py
@@ -5,6 +5,7 @@ from django.shortcuts import render_to_response, get_object_or_404
 from django.db.models import Q
 from django.core.paginator import Paginator
 from django_yaba.blog.models import Story, Article, Category, Links, Photo, Gallery
+from django_yaba.blog.multiquery import MultiQuerySet
 
 def category(request, slug):
     """Given a category slug, display all items in a category"""
@@ -17,7 +18,10 @@ def category(request, slug):
     return render_to_response("blog/story_list.html", {'posts':posts, 'ROOT_URL': ROOT_URL})
 
 def story_list(request):
-    paginator = Paginator(Story.objects.all().order_by('-created'), 5)
+    stories = Story.objects.all().order_by('-created')
+    galleries = Gallery.objects.all().order_by('-created')
+    front_page = MultiQuerySet(stories, galleries)
+    paginator = Paginator(front_page, 5)
     page = int(request.GET.get('page', '1'))
     posts = paginator.page(page)
     ROOT_URL = settings.ROOT_BLOG_URL
