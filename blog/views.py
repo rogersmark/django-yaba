b5c6b9355b60a0930885bf410f41a27858727f49
Mark <f4nt@f4ntasmic.com>
2009-06-21 14:31:25 -0500

    multiquery stuff now works, at least in testing :)

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,5 @@
+import logging
+from django_yaba.blog.multiquery import MultiQuerySet
 from django.conf import settings
 from django.core.exceptions import ObjectDoesNotExist
 from django.http import HttpResponseRedirect
@@ -5,6 +7,12 @@ from django.shortcuts import render_to_response, get_object_or_404
 from django.db.models import Q
 from django.core.paginator import Paginator
 from django_yaba.blog.models import Story, Article, Category, Links, Photo, Gallery
+
+LOG_FILENAME = '/tmp/yaba.out'
+logging.basicConfig(filename=LOG_FILENAME,level=logging.DEBUG,)
+
+def sort_by_date(x):
+    return x.created
 
 def category(request, slug):
     """Given a category slug, display all items in a category"""
@@ -18,7 +26,20 @@ def category(request, slug):
 
 def story_list(request):
     stories = Story.objects.all().order_by('-created')
-    paginator = Paginator(stories, 5)
+    galleries = Gallery.objects.all().order_by('-created')
+    temp = MultiQuerySet(stories, galleries)
+    front_page = []
+    for x in temp:
+        front_page.append(x)
+    
+    front_page.sort(key=sort_by_date, reverse=1)
+    logging.debug("Begin: front_page contents")
+    logging.debug(front_page)
+    logging.debug("Being: forloop of front_page content dates")
+    for x in front_page:
+        logging.debug(x.created)
+    logging.debug("End logging of front_page")
+    paginator = Paginator(front_page, 5)
     page = int(request.GET.get('page', '1'))
     posts = paginator.page(page)
     ROOT_URL = settings.ROOT_BLOG_URL
@@ -60,15 +81,17 @@ def story_id(request, story_id):
         return HttpResponseRedirect("/")
 
 def search(request):
+    """ searches across galleries, articles, and blog posts """
     ROOT_URL = settings.ROOT_BLOG_URL
     ROOT_URL = ROOT_URL.rstrip("/")
     if 'q' in request.GET:
         term = request.GET['q']
         post_list = Paginator(Story.objects.filter(Q(title__icontains=term) | Q(body__icontains=term)), 5)
         articles = Article.objects.filter(Q(title__icontains=term) | Q(body__icontains=term))
+        galleries = Gallery.objects.filter(Q(name__icontains=term) | Q(body__icontains=term))
         page = int(request.GET.get('page', '1'))
         posts = post_list.page(page)
-        return render_to_response("blog/story_search.html", {'posts': posts, "articles": articles, 'ROOT_URL': ROOT_URL})
+        return render_to_response("blog/story_search.html", {'posts': posts, "articles": articles, 'galleries': galleries, 'ROOT_URL': ROOT_URL})
 
     else:
        return HttpResponseRedirect('/')
