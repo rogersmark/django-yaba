d9fd9e3bbcd552830180ddb19c74f7a6506f5f91
Mark <f4nt@f4ntasmic.com>
2009-07-21 21:50:24 -0500

    added forgotten password

--- urls.py
+++ urls.py
@@ -4,6 +4,10 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
+    url(r'^admin/password_reset/$', 'django.contrib.auth.views.password_reset', name='password_reset'),
+    (r'^password_reset/done/$', 'django.contrib.auth.views.password_reset_done'),
+    (r'^reset/(?P<uidb36>[0-9A-Za-z]+)-(?P<token>.+)/$', 'django.contrib.auth.views.password_reset_confirm'),
+    (r'^reset/done/$', 'django.contrib.auth.views.password_reset_complete'),
     url(r'^admin/(.*)', admin.site.root),
     url(r'^comments/', include('django.contrib.comments.urls')),
     url(r'^', include('django_yaba.blog.urls')),
