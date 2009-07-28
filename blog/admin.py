e99848a41648c7b961794714d48ec6a0f1c5a51e
Mark <f4nt@f4ntasmic.com>
2009-07-27 21:26:50 -0500

    tinyMCE integrated. WYMEditor is now gone

--- blog/admin.py
+++ blog/admin.py
@@ -18,14 +18,12 @@ class ConfigurationAdmin(admin.ModelAdmin):
 class GalleryAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug': ('title',)}
     inlines = [PhotoInline]
-    form = GalleryAdminModelForm
 
 class CategoryAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug': ('label',)}
 
 class LinksAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug': ('label',)}
-    #list_display = ('label', 'site_link', 'url')
     list_filter = ('label', 'site_link')
     search_fields = ('label', 'site_link')
 
@@ -34,19 +32,15 @@ class StoryAdmin(admin.ModelAdmin):
     search_fields = ('title', 'content')
     list_filter = ('status', 'owner', 'created', 'modified')
     prepopulated_fields = {'slug': ('title',)}
-    form = ArticleAdminModelForm
 
 class ArticleAdmin(admin.ModelAdmin):
     list_display = ('title', 'owner', 'status', 'created', 'modified')
     search_fields = ('title', 'content')
     list_filter = ('status', 'owner', 'created', 'modified')
     prepopulated_fields = {'slug': ('title',)}
-    form = ArticleAdminModelForm
 
 admin.site.register(Story, StoryAdmin)
 admin.site.register(Category, CategoryAdmin)
-#admin.site.register(Item, ItemAdmin)
-#admin.site.register(Photo)
 admin.site.register(Links, LinksAdmin)
 admin.site.register(Article, ArticleAdmin)
 admin.site.register(Gallery, GalleryAdmin)
