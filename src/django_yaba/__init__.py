689b0798c92f3251c32d6a1c03627b6b0637797b
Mark Rogers <f4nt@f4ntasmic.com>
2009-10-19 17:51:27 -0500

    fixed a few dumb issues

--- src/django_yaba/__init__.py
+++ src/django_yaba/__init__.py
@@ -2,7 +2,9 @@ import os
 from django.conf import settings
 
 PROJECT_DIR = os.path.dirname(__file__)
-settings.MEDIA_ROOT = os.path.join(PROJECT_DIR, "media")
+if settings.MEDIA_ROOT is None:
+    settings.MEDIA_ROOT = os.path.join(PROJECT_DIR, "media")
+
 settings.TEMPLATE_DIRS += (
     os.path.join(PROJECT_DIR, "templates"),
     )
